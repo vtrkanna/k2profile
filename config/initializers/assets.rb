2cd8896874cb85346319f6153de321f2380cc79d
kanna <kannan@kreatio.com>
2014-06-26 19:58:46 +0530

    changes

---
+++ config/initializers/assets.rb
@@ -0,0 +1,3 @@
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( temp_last.css )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
--- config/routes.rb
+++ config/routes.rb
@@ -56,8 +56,8 @@ Rails.application.routes.draw do
 
   match '/' => 'home#home_main', via: :all
 
-  match 'auth/:provider/callback', to: 'sessions#create'
-  match 'auth/failure', to: redirect('/')
-  match 'signout', to: 'sessions#destroy', as: 'signout'
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/') , via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
 end
